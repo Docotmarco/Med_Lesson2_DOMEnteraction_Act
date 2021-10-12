5c8581a11f2bf4b9485461e80966fc917d2f2859
Docotmarco <marcodocot1234567@gmail.com>
2021-10-12 16:56:38 +0800

    create and adding method in js

--- activity3-Reactivity problem/react.js
+++ activity3-Reactivity problem/react.js
@@ -7,6 +7,41 @@ var app=Vue.createApp({
         // result:'Result'
       }
     },
+    methods:{
+        add(num){
+          this.counter=this.counter+num;
+           },
+       },
+       watch:{
+       counter(value){
+         
+          if(value >37 || value <37){
+           
+           const val= this;
+           setTimeout(function() {val.counter=0;},5000);
+          }else{
+             return 0;
+          }
+        }
+       },
+      computed:{
+        output(){  
+  
+          if(this.counter>37){
+            return 'Too much!';
+          }
+          else if(this.counter  < 37 && this.counter >0){
+            return 'Not there yet!';
+          }
+         
+          else if(this.counter===37){
+           return this.counter;
+          } 
+           else{
+            return 0;
+          }
+        }
+        
+      }
 
-    
 }); app.mount('.header');
