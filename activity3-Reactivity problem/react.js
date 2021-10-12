d442d8d6beed438c60b68157ced3a6e229fb49c1
Docotmarco <marcodocot1234567@gmail.com>
2021-10-12 16:54:21 +0800

    creating Vue instance to instantiate the function

---
+++ activity3-Reactivity problem/react.js
@@ -0,0 +1,12 @@
+var app=Vue.createApp({
+    data(){
+      return{
+        // tomuch:'Too much',
+        // notmuch:'Not there yet',  
+        counter:0,
+        // result:'Result'
+      }
+    },
+
+    
+}); app.mount('.header');
